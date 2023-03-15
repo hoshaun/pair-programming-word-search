6abb1472d26c66dc4ca7f5f667d311900f621f0f
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-15 15:39:00 -0400

    submit pair-programming-word-search solution

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,61 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if matrix is empty", function() {
+    const result = wordSearch([], 'SEINFELD');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'A', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'E', 'F', 'A', 'L', 'D'],
+      ['Y', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'L', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'D', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'D', 'F', 'A', 'L', 'D'],
+      ['Y', 'F', 'C', 'L', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'E', 'V', 'R', 'G'],
+      ['D', 'L', 'E', 'F', 'E', 'I', 'E', 'S'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'I', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'E', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'S', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/*
+Contributors:
+- Shaun Ho
+(Partner did not respond so I was told to do this alone)
+*/
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  const horizontalJoinBackwards = letters.map(ls => ls.reverse().join(''));
+  for (const l of horizontalJoinBackwards) {
+    if (l.includes(word)) return true;
+  }
+  
+  const verticalJoinBackwards = transpose(letters).map(ls => ls.reverse().join(''));
+  for (const l of verticalJoinBackwards) {
+    if (l.includes(word)) return true;
+  }
+  
+  return false;
+};
+  
+const transpose = function(matrix) {
+  let array = [];
+    
+  for (const row of matrix) {
+    for (const col in row) {
+    
+      if (!array[col]) {
+        array.push([]);
+      }
+    
+      array[col].push(row[col]);
+    }
+  }
+    
+  return array;
+};
+  
+module.exports = wordSearch;
